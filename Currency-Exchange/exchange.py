a3a5b7db8b9242bb2ed0a257150ad8ceb090f56f
Victor Daniel Herrera Ayos <pasiondelbarza@gmail.com>
2022-04-25 12:55:10 -0500

    Punto 2: Terminado

--- Currency-Exchange/exchange.py
+++ Currency-Exchange/exchange.py
@@ -6,6 +6,10 @@ def exchange_money(budget, exchange_rate):
     :return: float - exchanged value of the foreign currency you can receive.
     """
     return budget / exchange_rate
+# exchange_money()'s outputs
+money = exchange_money(15000,3927)
+print(f'Exchange money is {money}')
+
 def get_change(budget, exchanging_value):
     """
  
@@ -14,6 +18,11 @@ def get_change(budget, exchanging_value):
     :return: float - amount left of your starting currency after exchanging.
     """
     return budget - exchanging_value
+
+# get_change()'s outputs
+money = get_change(15000,3927)
+print(f'Your change is {money}')
+
 def get_value_of_bills(denomination, number_of_bills):
     """
  
@@ -22,6 +31,11 @@ def get_value_of_bills(denomination, number_of_bills):
     :return: int - total value of bills you now have.
     """
     return denomination * number_of_bills
+
+# get_value_of_bills()'s outputs
+value = get_value_of_bills(15000,3)
+print(f'Your value of bills is {value}')
+
 def get_number_of_bills(budget, denomination):
     """
  
@@ -30,6 +44,11 @@ def get_number_of_bills(budget, denomination):
     :return: int - number of bills after exchanging all your money.
     """
     return int(budget / denomination)
+
+# get_number_of_bills()'s outputs
+bills = get_number_of_bills(15000,5)
+print(f'Your number of bills is {bills}')
+
 def exchangeable_value(budget, exchange_rate, spread, denomination):
     """
  
@@ -44,6 +63,11 @@ def exchangeable_value(budget, exchange_rate, spread, denomination):
     exchanged_number = new_currency / denomination
     max_value = int(exchanged_number) * denomination
     return max_value
+
+# exchangeable_value()'s outputs
+exchangeable = exchangeable_value(15000,3927,2,1)
+print(f'Your exchangeable value is {exchangeable}')
+
 def non_exchangeable_value(budget, exchange_rate, spread, denomination):
     """
  
@@ -58,3 +82,7 @@ def non_exchangeable_value(budget, exchange_rate, spread, denomination):
     exchanged_number = new_currency / denomination
     max_value = int(exchanged_number) * denomination
     return int(new_currency - max_value)
+
+# non_exchangeable_value()'s outputs
+non_exchangeable = non_exchangeable_value(15000,300,1,1)
+print(f'Your non exchangeable value is {non_exchangeable}')
